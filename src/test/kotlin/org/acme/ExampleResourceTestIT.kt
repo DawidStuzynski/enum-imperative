8d0d944b4f502cbf66f3dc424314a3aab0be608b
Dawid Stuzynski <dawid.stuzynski@gmail.com>
2023-11-07 09:39:32 +0100

    Single enum warning

--- src/test/kotlin/org/acme/ExampleResourceTestIT.kt
+++ src/test/kotlin/org/acme/ExampleResourceTestIT.kt
@@ -1,14 +1,10 @@
 package org.acme
 
-import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.QuarkusTest
 import io.restassured.module.kotlin.extensions.Extract
 import io.restassured.module.kotlin.extensions.Then
 import io.restassured.module.kotlin.extensions.When
 import org.junit.jupiter.api.Test
-import org.skyscreamer.jsonassert.JSONAssert
-import org.skyscreamer.jsonassert.JSONCompareMode
-import org.testcontainers.junit.jupiter.Testcontainers
 
 @QuarkusTest
 class ExampleResourceTestIT {
